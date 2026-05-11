47408e03df3edde93580c8db213181becf813bfb
gemirrrr <trembala1719@gmail.com>
2026-05-11 14:52:13 -0300

    Add files via upload

---
+++ assets/js/link.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+});
